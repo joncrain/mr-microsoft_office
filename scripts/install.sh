bb0370d41a39da88af78a38e4c6995a48d0c7d93
Jon Crain <joncrain@gmail.com>
2018-11-29 09:37:56 -0500

    Fixing other scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,19 +4,19 @@
 CTL="${BASEURL}index.php?/module/microsoft_office/"
 
 # Get the scripts in the proper directories
-${CURL} "${CTL}get_script/microsoft_office.sh" -o "${MUNKIPATH}preflight.d/microsoft_office.sh"
+${CURL} "${CTL}get_script/microsoft_office.py" -o "${MUNKIPATH}preflight.d/microsoft_office.py"
 
 # Check exit status of curl
 if [ $? = 0 ]; then
 	# Make executable
-	chmod a+x "${MUNKIPATH}preflight.d/microsoft_office.sh"
+	chmod a+x "${MUNKIPATH}preflight.d/microsoft_office.py"
 
 	# Set preference to include this file in the preflight check
-	setreportpref "microsoft_office" "${CACHEPATH}microsoft_office.txt"
+	setreportpref "microsoft_office" "${CACHEPATH}microsoft_office.json"
 
 else
 	echo "Failed to download all required components!"
-	rm -f "${MUNKIPATH}preflight.d/microsoft_office.sh"
+	rm -f "${MUNKIPATH}preflight.d/microsoft_office.py"
 
 	# Signal that we had an error
 	ERR=1
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Remove microsoft_office script
-rm -f "${MUNKIPATH}preflight.d/microsoft_office.sh"
+rm -f "${MUNKIPATH}preflight.d/microsoft_office.py"
 
 # Remove microsoft_office.txt file
-rm -f "${MUNKIPATH}preflight.d/cache/microsoft_office.txt"
+rm -f "${MUNKIPATH}preflight.d/cache/microsoft_office.json"
